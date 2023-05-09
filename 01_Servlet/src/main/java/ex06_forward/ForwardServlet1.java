35b6ec3b77d83435bc28516a8a50f078563d1e0d
DKoding <foejakzm@naver.com>
2023-05-09 19:38:25 +0900

    0509

--- 01_Servlet/src/main/java/ex06_forward/ForwardServlet1.java
+++ 01_Servlet/src/main/java/ex06_forward/ForwardServlet1.java
@@ -26,8 +26,12 @@ public class ForwardServlet1 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+			String model = request.getParameter("model");
+			System.out.println("ForwardServlet1 : " + model);
+			
+			request.getRequestDispatcher("/ForwardServlet2").forward(request,  response);
+			
 	}
 
 	/**
